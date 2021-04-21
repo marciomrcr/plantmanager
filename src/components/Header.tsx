764f39957fe6e78bb15ca87544a4be88f4ca411c
Márcio Rodrigues <marcio.mrcr@outlook.com>
2021-04-21 17:20:38 -0300

    Criação component EnviromentButton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,29 @@ import {
     View,
     Text,
     StyleSheet,
-    Image
+    Image,
+    Dimensions
 } from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 
+import userImg from '../assets/watering.png'
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 export function Header(){
     return(
         <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>
+                    Olá
+                    </Text>
+                <Text style={styles.userName}>
+                    Márcio
+                    </Text>
+            </View>
+
+            <Image source={userImg} style={styles.image}/>
+            
 
         </View>
     )
@@ -23,6 +38,29 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingVertical: 20,
-        backgroundColor: colors.red
-            }
+        marginTop: getStatusBarHeight(),
+},
+
+greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+
+},
+
+userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40
+
+    
+},
+
+image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40
+
+}
 })
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -2,16 +2,45 @@ import React from 'react'
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    FlatList
 } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
 
+import { EnviromentButton } from '../components/EnviromentButton'
 import { Header} from '../components/Header'
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 export function PlantSelect(){
     return(
         <View style={styles.container}>
-            <Header />
+            <View style={styles.header}>
+                <Header />
+
+                <Text style={styles.title}>
+                Em qual ambiente
+                </Text >
+                <Text style={styles.subTitle}>
+                você vai colococar sua planta?                
+                </Text>
+            </View>
+
+            <View>
+                <FlatList
+                data={[1,2,3,4,5]}
+                renderItem={({ item }) => (
+                    <EnviromentButton
+                    title="Cozinha" 
+                    active
+                    /> 
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.enviromentList}
+                />
+            </View>
+                      
         </View>
     )
 }
@@ -20,6 +49,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.background
+    },
+
+    header: {
+        paddingHorizontal: 30
+    },
+
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+
+    subTitle: {
+        fontSize: 17,
+        fontFamily: fonts.heading,
+        color: colors.heading,        
+        lineHeight: 20        
+    },
+    enviromentList:{
+        height: 40,
+        justifyContent: 'center',
+        paddingBottom: 5,
+        marginLeft: 32,
+        marginVertical: 32
     }
+
 })
 
